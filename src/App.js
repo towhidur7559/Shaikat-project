c9d7e0d1b487ffbee2918476ad5be6a3bfd749c2
Towhidur Rahman <91563896+towhidur7559@users.noreply.github.com>
2022-11-30 09:02:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [Find_color1, setColor1] = useState('');
+  const [Find_color2, setColor2] = useState('');
+  const [Find_color3, setColor3] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+    <div>
+      <div className="div">
+        <header>
+          <h1 className='h'>
+            Shafiul Alam
+          </h1>
+        </header>
+      </div>
+      <div className="main">
+      <div className="input1">
+        <div className="App">
+          <textarea style={{ backgroundColor: Find_color1, height: '30vh' }}></textarea>
+        </div>
+        <div className="App sp">
+          <input  type="text" value={Find_color1} onChange={(s) => setColor1(s.target.value)}/>
+        </div>
+      </div>
+      <div className="input2">
+        <div className="App">
+          <textarea style={{ backgroundColor: Find_color2, height: '30vh' }}></textarea>
+        </div>
+        <div className="App sp">
+          <input type="text" value={Find_color2} onChange={(s) => setColor2(s.target.value)}/>
+        </div>
+      </div>
+      <div className="input3">
+        <div className="App">
+          <textarea style={{ backgroundColor: Find_color3, height: '30vh' }}></textarea>
+        </div>
+        <div className="App sp">
+          <input  type="text" value={Find_color3} onChange={(s) => setColor3(s.target.value)}/>
+        </div>
+      </div>
+      </div>
+      
     </div>
+
+
+
   );
 }
 
